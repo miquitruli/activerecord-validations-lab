2ecf5a4fbcd6c1b3aefe176ff095f8b7c6bcf3b3
Micaella Colombo <micaelc@g.clemson.edu>
2021-12-28 10:09:40 -0500

    commit tree c2b6b61475d2ee368eaee35eecb0f3ca7ab3e008

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
+    validate :is_clickbait?
+    
+    CLICKBAIT_PATTERNS = [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
+
+    def is_clickbait?
+        if CLICKBAIT_PATTERNS.none? {|p| p.match title }
+            errors.add(:title, "need to be clickbait")
+        end
+    end
 end
